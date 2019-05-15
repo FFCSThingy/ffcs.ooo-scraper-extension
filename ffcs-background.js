be996d8602ef1ff99c4f01a561349b95eff1f7d0
Namit Nathwani <namsnath@gmail.com>
2019-05-16 02:17:37 +0530

    Added POST request for data

--- ffcs-background.js
+++ ffcs-background.js
@@ -1,10 +1,16 @@
 chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse) {
+    async function(message, sender, sendResponse) {
+		$.post('http://localhost:3001/ext/processExtensionData', message, function(data) {
+			// sendResponse(message.url);
+			// var resp = await data;	
+			// sendResponse("OK");
+		});
+
         // send `message` to server;
         // ajax
         // something
 		// TODO: add some sort of auth
-		sendResponse(message);
-		// sendResponse("OK");
+		// sendResponse(data);
+		sendResponse("OK");
     }
 );
